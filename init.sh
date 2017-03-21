9b6eff312a09f9c26b8b7e74082439bf78c79d5a
Mosa <mosaalhusseini@hotmail.com>
2017-03-21 12:21:09 +0100

    [TASK] updated init script for t3kit7 (#5)

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ if [ ! -d "db" ]; then
 fi
 cd db
 if [ ! -d .git ]; then
-    git clone https://github.com/t3kit/t3kit_db.git .
+    git clone -b t3kit7 https://github.com/t3kit/t3kit_db.git .
 fi;
 cd ..
 
@@ -15,9 +15,9 @@ if [ ! -d "site" ]; then
 fi
 cd site
 if [ ! -d .git ]; then
-    git clone https://github.com/t3kit/t3kit.git .
+    git clone -b t3kit7 https://github.com/t3kit/t3kit.git .
     git submodule update --init --recursive
 fi;
 cd TYPO3.CMS
-composer install
+composer install --no-dev
 cd ../../..
